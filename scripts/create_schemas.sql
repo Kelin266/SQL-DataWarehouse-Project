19c39b85495a070f6aa7935ea37842df4ad58418
Kelin Patel <kelinp007@gmail.com>
2026-02-03 21:07:54 +0530

    Add files via upload

---
+++ scripts/create_schemas.sql
@@ -0,0 +1,28 @@
+
+-- Use master
+USE master;
+
+-- Drop and create the 'DataWarehouse' database
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+--Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
